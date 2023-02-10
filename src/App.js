f2a51d2593afb57dafb2c65c2462b18cbd188e4a
GrishithaV <grishithavinta@gmail.com>
2023-02-10 14:00:46 +0530

    Added footercomponents file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navigationbar from "./components/Navbarcompnents";
+import Footerbar from './components/Footercomponents';
 
 function App() {
   return (
     <div>
       <Navigationbar/>
+      <Footerbar/>
     </div>
   );
 }
---
+++ src/components/Footercomponents.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+
+const Footerbar = () => {
+    return (
+      <div >
+     NGO
+      
+      </div>
+    )
+  }
+  
+  export default Footerbar;
+  
