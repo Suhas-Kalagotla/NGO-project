c63d46fd0054348219c45e261bbb1e56acc044ff
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2023-07-23 21:53:35 +0530

    commiting the untracked footer files

---
+++ client/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import react from 'react'; 
+import "./footer.css"; 
+const Footer = () =>{
+  return(
+    <div className="footer">
+      <hr></hr>
+      <p>Developed by VIHAAN Education</p>
+    </div>
+  ) 
+}
+
+export default Footer; 
