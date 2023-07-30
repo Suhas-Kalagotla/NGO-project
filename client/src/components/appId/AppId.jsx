664184dfa3ce5f9ca8339b6a42d79c374a017a0a
Suhas-Kalagotla <suhaskalagotla5@gmail.com>
2023-07-31 02:05:56 +0530

    created component for each application

--- client/src/components/appId/AppId.jsx
+++ client/src/components/appId/AppId.jsx
@@ -4,11 +4,24 @@ import axios from "axios";
 import { url } from "../../utils/url";
 import { useEffect,useState } from "react";
 import Loading from "../loading/Loading"; 
+import Left from "../../images/left.svg"; 
 
 const AppId = () =>{
   const {id} = useParams(); 
   const [application,setApplication] = useState(null); 
+  const [color, setColor] = useState({statusColor:"",volunteerColor:""}) ; 
   const user = JSON.parse(localStorage.getItem("user")); 
+  const findColor = () =>{
+    const status = application?.status; 
+    if(status === "Pending"){
+      setColor({...color,statusColor:"Red"}); 
+    }else if(status ==="Validated"){
+      setColor({...color,statusColor:"orange"}); 
+    }else if(status ==="Approved"){
+      setColor({...color,statusColor:"green"}); 
+    }
+
+  }
   const fetchApplication = async () =>{
     try{
       const response = await axios.post(`${url}/admin/application/getDetails`,
@@ -27,22 +40,34 @@ const AppId = () =>{
   useEffect(()=>{
     fetchApplication(); 
   },[]); 
+  useEffect(()=>{
+    findColor(); 
+  },[application]); 
 
   const navigate = useNavigate(); 
   const handleGoBack = ()=>{
     navigate(-1); 
-    console.log("hello")
   }
   return(
   <div className="reportContainer">
   {
     application ? (
-      <>
-    <p>
-      user id : {id}
-      and email is : {application.email}
-    </p>
-    <button onClick={handleGoBack}>Go back</button>
+    <>
+    <div className="appHeading">
+      <button onClick={handleGoBack} className="back"><img src ={Left}/></button>
+      <p>Updated At : {application.updatedAt.slice(0,10)}</p>
+      <p>Applied On : {application.createdAt.slice(0,10)}</p>
+      <div className="statusContainer">
+        <p>Status :</p>
+        <p className="status" style={{backgroundColor:color.statusColor}}>{application.status}</p>
+      </div>
+      <div className="volunteerContainer">
+        <p>Volunteer :</p>
+        <p className="volunteer">Not Assigned</p>
+      </div>
+    </div>
+      <hr></hr>
+    <div className ="appBody"></div>
     </>
   ):
   (
